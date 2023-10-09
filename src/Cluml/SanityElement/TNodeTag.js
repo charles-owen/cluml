9d10a8bb8834d83d00cce71bd86d0a00c18f3cc8
BlakeBeMent <blakebemnt@gmail.com>
2023-10-09 19:39:20 -0400

    Added check for association role's first character being a lowercase letter.

--- src/Cluml/SanityElement/TNodeTag.js
+++ src/Cluml/SanityElement/TNodeTag.js
@@ -1,5 +1,7 @@
 import { SanityElement } from "./SanityElement";
-import { SPACES_RX, NON_ALPHANUMERIC_RX} from "../Utility/Name";
+import { SPACES_RX, NON_ALPHANUMERIC_RX, NON_ALPHABETICAL_RX} from "../Utility/Name";
+
+const LOWERCASE_RX = /[a-z]/;
 
 export class TNodeTag extends SanityElement {
     processSanityCheck() {
@@ -13,6 +15,12 @@ export class TNodeTag extends SanityElement {
         if (NON_ALPHANUMERIC_RX.test(this.elementValue))
         {
             errors.push(`Role <a>${this.elementValue}</a> contains non-alphanumeric characters`);
+        }
+        if (this.elementValue.length > 0)
+        {
+            const lowerCase = this.elementValue[0].match(LOWERCASE_RX);
+            if (lowerCase === null || lowerCase.length === 0)
+                errors.push(`Role <a>${this.elementValue}</a> is not in the camelCase format`);
         }
         return errors;
     }
