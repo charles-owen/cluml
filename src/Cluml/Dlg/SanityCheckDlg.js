a81597edab939364afed64f317bb8047070fbf53
BlakeBeMent <bementbl@msu.edu>
2023-10-23 17:13:26 -0400

    Added sanity check for composition cycles when opening the dialog box.
    Right now it notifies when a cycle has been detected. I can later modify it so that it captures the path of the cycle.

--- src/Cluml/Dlg/SanityCheckDlg.js
+++ src/Cluml/Dlg/SanityCheckDlg.js
@@ -1,5 +1,6 @@
 import {Dialog} from './Dialog';
 import {SanityElement} from "../SanityElement/SanityElement";
+import {SanityErrorInfo} from "../SanityElement/SanityErrorInfo";
 
 /**
  * Sanity Check dialog box.
@@ -39,6 +40,26 @@ export const SanityCheckDlg = function(main) {
             }
         }
 
+        // Check for composition cycles
+        const classes = main.currentView().diagram.getComponentsByType("Class");
+        const visited = new Set();
+        let recursiveVisited = new Set();
+        let hasCycle = false;
+        for (const classObj of classes) {
+            if (visited.has(classObj)) {
+                continue;
+            }
+            recursiveVisited.clear();
+            hasCycle = hasCycle || isCyclic(classObj, visited, recursiveVisited);
+            if (hasCycle)
+                break;
+        }
+        if (hasCycle) {
+            errorTbl.appendChild(new SanityErrorInfo("9999", "Diagram",
+                "", "Composition cycle detected").HTMLRepresentation);
+            errorCount += 1;
+        }
+
         const h2 = document.createElement('h2');
         h2.textContent = `(${errorCount}) errors have been detected`;
         html.appendChild(h2);
@@ -47,6 +68,41 @@ export const SanityCheckDlg = function(main) {
 
         this.contents(html, "Cluml Sanity Check");
         Dialog.prototype.open.call(this);
+    }
+
+
+    /**
+     * Function to find composition cycles
+     * @param node Class node from the current diagram
+     * @param visited set of classes already visited
+     * @param recursiveVisited set to keep track of recursive visits. If an element is already in this set, then
+     * there is a cycle
+     * @returns {boolean}
+     */
+    const isCyclic = function(node, visited, recursiveVisited)
+    {
+        if (node === undefined || node === null)
+        {
+            return false;
+        }
+        if (recursiveVisited.has(node)) {
+            return true;
+        }
+        let hasCycle = false;
+        if (!visited.has(node))
+        {
+            visited.add(node);
+            recursiveVisited.add(node);
+
+            for (const tNode of node.attachedTNodes) {
+                if (tNode.association.fileLbl === "Composition" && !tNode.isTail) {
+                    hasCycle = hasCycle || isCyclic(tNode.association.nodes.end.attachedTo, visited, recursiveVisited);
+
+                }
+            }
+            recursiveVisited.delete(node);
+        }
+        return hasCycle;
     }
 
     const isAlphaNumeric = function(s) {
