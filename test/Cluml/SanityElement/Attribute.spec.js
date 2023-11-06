62dab7e0c1c8960e56ee8b2609575b25dd28224b
BlakeBeMent <bementbl@msu.edu>
2023-11-06 16:18:14 -0500

    Added unit tests to attributes and operations for missing properties.

--- test/Cluml/SanityElement/Attribute.spec.js
+++ test/Cluml/SanityElement/Attribute.spec.js
@@ -1,4 +1,5 @@
 import {Attribute} from '../../../src/Cluml/SanityElement/Attribute';
+import {Class} from "../../../src/Cluml/Components/Class";
 
 describe('Attribute', function() {
     it('Should construct', function() {
@@ -27,5 +28,31 @@ describe('Attribute', function() {
         expect(attribute.visibility).toEqual("+");
         expect(attribute.name).toEqual("attribute");
         expect(attribute.type).toEqual("String");
+    });
+    it ('Should detect missing properties', function() {
+        const missingVisibility = "attribute: String";
+        const missingName = "+: String";
+        const missingType = "+attribute:";
+
+        let attribute = new Attribute(missingVisibility);
+
+        Class.prototype.showVisibility = true;
+        let messages = attribute.processSanityCheck();
+        expect(messages.length).toEqual(1);
+        expect(messages[0].description).toEqual("Visibility missing");
+
+        Class.prototype.showVisibility = false;
+        messages = attribute.processSanityCheck();
+        expect(messages.length).toEqual(0);
+
+        attribute = new Attribute(missingName);
+        messages = attribute.processSanityCheck();
+        expect(messages.length).toEqual(1);
+        expect(messages[0].description).toEqual("Name missing");
+
+        attribute = new Attribute(missingType);
+        messages = attribute.processSanityCheck();
+        expect(messages.length).toEqual(1);
+        expect(messages[0].description).toEqual("Type missing");
     });
 });
--- test/Cluml/SanityElement/Operation.spec.js
+++ test/Cluml/SanityElement/Operation.spec.js
@@ -1,4 +1,6 @@
 import {Operation} from '../../../src/Cluml/SanityElement/Operation';
+import {operation} from "../../../src/Cluml/SanityElement/operation";
+import {Class} from "../../../src/Cluml/Components/Class";
 
 describe('Operation', function() {
     it('Should construct', function() {
@@ -27,5 +29,44 @@ describe('Operation', function() {
        let operation = new Operation('  + operation( param1 :  Int,,,,,,)  :     String     ');
         expect(operation.elementValue).toEqual("+operation(param1: Int): String");
         expect(operation.parameters[0]).toEqual(["param1", "Int"]);
+    });
+    it ('Should detect missing properties', function() {
+        const missingVisibility = "operation(): String";
+        const missingName = "+(): String";
+        const missingType = "+operation():";
+        const missingParamName = "+operation(: String): String";
+        const missingParamType = "+operation(param:): String";
+
+
+        let operation = new Operation(missingVisibility);
+
+        Class.prototype.showVisibility = true;
+        let messages = operation.processSanityCheck();
+        expect(messages.length).toEqual(1);
+        expect(messages[0].description).toEqual("Visibility missing");
+
+        Class.prototype.showVisibility = false;
+        messages = operation.processSanityCheck();
+        expect(messages.length).toEqual(0);
+
+        operation = new Operation(missingName);
+        messages = operation.processSanityCheck();
+        expect(messages.length).toEqual(1);
+        expect(messages[0].description).toEqual("Name missing");
+
+        operation = new Operation(missingType);
+        messages = operation.processSanityCheck();
+        expect(messages.length).toEqual(1);
+        expect(messages[0].description).toEqual("Type missing");
+
+        operation = new Operation(missingParamName);
+        messages = operation.processSanityCheck();
+        expect(messages.length).toEqual(1);
+        expect(messages[0].description).toEqual("Parameter name missing");
+
+        operation = new Operation(missingParamType);
+        messages = operation.processSanityCheck();
+        expect(messages.length).toEqual(1);
+        expect(messages[0].description).toEqual("Parameter type missing");
     });
 });
